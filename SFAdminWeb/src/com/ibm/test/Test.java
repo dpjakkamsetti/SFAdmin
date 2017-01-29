a188e02587d2921cc1196a32305bd35ea7185e12
durgaj2 <durgaj2@Durga>
2017-01-29 15:54:11 +0530

    second version added.

--- SFAdminWeb/src/com/ibm/test/Test.java
+++ SFAdminWeb/src/com/ibm/test/Test.java
@@ -3,5 +3,6 @@ package com.ibm.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Git test 1...");
+		System.out.println("Developer 2 modified the code...");
 	}
 }
