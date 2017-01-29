deb4fbdb989fb4b102b3a08609ba728eac37f837
durgaj2 <durgaj2@Durga>
2017-01-29 16:08:57 +0530

    removed sop.

--- SFAdminWeb/src/com/ibm/test/Test.java
+++ SFAdminWeb/src/com/ibm/test/Test.java
@@ -3,6 +3,5 @@ package com.ibm.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Git test 1...");
-		System.out.println("Developer 2 modified the code...");
 	}
 }
