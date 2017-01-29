6eb9c70bb5ab9b7fe35e30b280255968ece472ea
durgaj2 <durgaj2@Durga>
2017-01-29 16:11:56 +0530

    added for loop.

--- SFAdminWeb/src/com/ibm/test/Test.java
+++ SFAdminWeb/src/com/ibm/test/Test.java
@@ -4,5 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Git test 1...");
 		System.out.println("Developer 2 modified the code...");
+		for(int i=0; i<10; i++){
+			System.out.println("Durga");
+		}
 	}
 }
